13a768bd199bf0c9ad49c2648040589604f2e5a3
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-02 13:56:41 +0200

    extended readme and corrected strength calculation

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Password Strength Calculator
 
-password_strength.py calculates the strength of the given password using the following rules:
+password_strength.py calculates the strength (1 - weakest, 10 - strong) 
+of the given password using the following rules:
 
 Requirements:
 -------------
-Minimum 5 characters in length (maximum 20)
+Minimum 5 characters in length (maximum 10)
 Contains 3/4 of the following items:
 - Uppercase Letters
 - Lowercase Letters
@@ -28,6 +29,18 @@ Consecutive Uppercase Letters	-(n*2)
 Consecutive Lowercase Letters	-(n*2)	
 Consecutive Numbers		        -(n*2)
 
+# to run program:
+```
+python password_strength.py
+enter password to check (5 <= length <= 10): qwe123
+password strength is 4
+```
+# to stop program enter empty string (just enter):
+```
+enter password to check (5 <= length <= 10):
+
+Process finished with exit code 0
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-# -*- coding: utf-8
 
 import re
 
 
-def m1(pwd):  # number of upper letters
+def count_upper_chars(pwd):
     return sum(1 for c in pwd if c.isupper())
 
 
-def m2(pwd):  # number of lower letters
+def count_lower_chars(pwd):  # number of lower letters
     return sum(1 for c in pwd if c.islower())
 
 
-def m3(pwd):  # number of digits
+def count_digits(pwd):  # number of digits
     return sum(1 for c in pwd if c.isdigit())
 
 
-def m4(pwd):  # number of special symbols
+def count_symbols(pwd):  # number of special symbols
     return sum(1 for c in pwd if c in "$#@!%")
 
 
-def m5(pwd):  # check if there are letter (upper and lower), digits and symbols
-    return (m1(pwd) > 0) + (m2(pwd) > 0) + (m3(pwd) > 0) + (m4(pwd) > 0)
+def count_requirements(pwd):  # check if there are upper and lower letters, digits and symbols
+    return (count_upper_chars(pwd) > 0) + (count_lower_chars(pwd) > 0) + (count_digits(pwd) > 0) + (
+        count_symbols(pwd) > 0)
 
 
-def m10(pwd):  # calculate number of consecutive uppercase letters
+def repeating_upper_chars(pwd):  # calculate number of consecutive uppercase letters
     dup = 0
     for m in re.finditer(r'([A-Z])\1*', pwd):
         if len(m.group(0)) > 1:
@@ -32,7 +32,7 @@ def m10(pwd):  # calculate number of consecutive uppercase letters
     return dup
 
 
-def m11(pwd):  # calculate number of consecutive lowercase letters
+def repeating_lower_chars(pwd):  # calculate number of consecutive lowercase letters
     dup = 0
     for m in re.finditer(r'([a-z])\1*', pwd):
         if len(m.group(0)) > 1:
@@ -40,7 +40,7 @@ def m11(pwd):  # calculate number of consecutive lowercase letters
     return dup
 
 
-def m12(pwd):  # calculate number of consecutive digits
+def repeating_digits(pwd):  # calculate number of consecutive digits
     dup = 0
     for m in re.finditer(r'([0-9])\1*', pwd):
         if len(m.group(0)) > 1:
@@ -63,33 +63,34 @@ def digits_only(pwd):
 
 
 def calculate_strength(password: "str") -> "int":
+    max_strength = 130
     # add strengths
     strength = len(password) * 4
-    strength += (len(password) - m1(password)) * 2
-    strength += (len(password) - m2(password)) * 2
-    strength += m3(password) * 4
-    strength += m4(password) * 6
-    strength += m5(password) * 2
+    strength += (len(password) - count_upper_chars(password)) * 2
+    strength += (len(password) - count_lower_chars(password)) * 2
+    strength += count_digits(password) * 4
+    strength += count_symbols(password) * 6
+    strength += count_requirements(password) * 2
 
     # subtract weaknesses
     strength -= letters_only(password)
     strength -= digits_only(password)
-    strength -= m10(password) * 2
-    strength -= m11(password) * 2
-    strength -= m12(password) * 2
+    strength -= repeating_digits(password) * 2
+    strength -= repeating_lower_chars(password) * 2
+    strength -= repeating_upper_chars(password) * 2
 
-    return strength
+    return (strength * 10) // max_strength  # return strength in scale from 1 to 10
 
 
 def main():
     while True:
-        pwd = input("enter password to check (5 <= length <= 20): ").strip()
+        pwd = input("enter password to check (5 <= length <= 10): ").strip()
         if pwd:
-            if 4 < len(pwd) < 21:
+            if 5 <= len(pwd) <= 10:
                 if ' ' in pwd:
                     print("no spaces allowed inside password")
                 else:
-                    print("password strength is {} (min=17,  max=270)".format(calculate_strength(pwd)))
+                    print("password strength is {}".format(calculate_strength(pwd)))
             else:
                 print("invalid password length {}".format(len(pwd)))
         else:
